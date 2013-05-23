6da38d7cc4c09c877d3e8f19bd5541e6b7b44cf6
Sen Wang <moli.wang@gmail.com>
2013-05-23 12:09:28 +0800

    init

---
+++ WpfApplication1/ColorWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using System.IO;
+
+namespace WpfApplication1
+{
+
+    public partial class ColorWindow : Window
+    {
+        KinectSensor kinect;
+        public ColorWindow(KinectSensor sensor) : this()
+        {
+            kinect = sensor;
+        }
+        public ColorWindow()
+        {
+            InitializeComponent();
+            Loaded += ColorWindow_Loaded;
+            Unloaded += ColorWindow_Unloaded;
+        }
+        void ColorWindow_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (kinect != null)
+            {
+                kinect.ColorStream.Disable();
+                kinect.ColorFrameReady -= myKinect_ColorFrameReady;
+                kinect.Stop();
+            }
+        }
+        private WriteableBitmap _ColorImageBitmap;
+        private Int32Rect _ColorImageBitmapRect;
+        private int _ColorImageStride;
+        void ColorWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (kinect != null)
+            {
+                ColorImageStream colorStream = kinect.ColorStream;
+
+                colorStream.Enable();
+                kinect.ColorFrameReady += myKinect_ColorFrameReady;
+
+                _ColorImageBitmap = new WriteableBitmap(colorStream.FrameWidth,colorStream.FrameHeight, 96, 96,
+                                                        PixelFormats.Bgr32, null);
+                _ColorImageBitmapRect = new Int32Rect(0, 0, colorStream.FrameWidth,colorStream.FrameHeight);
+                _ColorImageStride = colorStream.FrameWidth * colorStream.FrameBytesPerPixel;
+                ColorData.Source = _ColorImageBitmap;
+
+                kinect.Start();
+            }
+        }
+
+        void myKinect_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame frame = e.OpenColorImageFrame())
+            {
+                if(frame == null)
+                    return ;
+                
+                byte[] pixelData = new byte[frame.PixelDataLength];
+                frame.CopyPixelDataTo(pixelData);
+                _ColorImageBitmap.WritePixels(_ColorImageBitmapRect, pixelData,_ColorImageStride, 0);
+            }
+        }
+
+        private void ColorData_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string filename = NewFileName();
+            SaveToFile(filename);
+        }
+
+        private int i = 0;
+        public string NewFileName()
+        {
+            i++;
+
+            string mypicsfolder =
+                           Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            string fileName = mypicsfolder + "\\pic_" + i + ".png";
+
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            return fileName;
+
+        }
+        public void SaveToFile(string fileName)
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.CreateNew))
+            {
+                BitmapSource image = (BitmapSource)ColorData.Source;
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(image));
+                encoder.Save(fs);
+            }
+        }
+
+    }
+}
